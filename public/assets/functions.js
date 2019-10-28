21e59c097173c8e42ee18e8f8ed0cb86cf89d8bf
Joseph García <joseph@krakenhub.net>
2019-10-28 10:53:59 -0600

    calculadora terminada

---
+++ public/assets/functions.js
@@ -0,0 +1,71 @@
+
+$(document).ready(function () {
+    let instancia_monto = $("#monto-deseado");
+    let instancia_tasa = $("#tasa-anual");
+    let tasaAnual = 8.9;
+    let comApertura = 348;
+    let cat = 15.71;
+    let plazo = 12;
+
+
+
+    function updateData(monto, meses, comision, tasa, cat) {
+        let mensualidad = (monto + per(monto, tasa) - comision) / meses;
+        let totalPagar = ((monto - comision) + (per(monto, tasa)));
+        $(".datos span.comision").text(comision);
+        $(".datos span.cat").text(cat);
+        $(".datos span.neto-depositado").text(monto - comision);
+        $(".datos span.mensualidad").text(mensualidad.toFixed(2));
+        $(".datos span.total-pagar").text(totalPagar);
+
+    }
+    console.log("ready!");
+    instancia_monto.ionRangeSlider({
+        skin: "round",
+        min: 10000,
+        max: 100000,
+        step: 5000,
+        prefix: "$",
+        onStart: function (data) {
+            console.log(data);
+            updateData(data.from, plazo, comApertura, tasaAnual, cat)
+        },
+        onFinish: function (data) {
+            console.log(data.from)
+            updateData(data.from, plazo, comApertura, tasaAnual, cat)
+        }
+
+
+    });
+    instancia_tasa.ionRangeSlider({
+        min: 8.9,
+        max: 26.7,
+        step: 8.9,
+        skin: "round",
+        block: true,
+        postfix: "%"
+    });
+
+    $(".plazos a.button").click(function (e) {
+        let plazos =  $(this).data("plazo");
+        tasa = new Array();
+        tasa[12] = 8.9;
+        tasa[24] = 17.8;
+        tasa[36] = 26.7;
+
+        e.preventDefault();
+        $('.plazos a.button').removeClass('seleccionado');
+        $(this).addClass('seleccionado');
+        console.log($(this).data("plazo"));
+        updateData(instancia_monto.data("ionRangeSlider").old_from,plazos,comApertura,tasaAnual,cat);
+        var o = {
+            from: tasa[plazos]
+        }
+
+        instancia_tasa.data("ionRangeSlider").update(o);
+
+    });
+});
+function per(monto, porcentaje) {
+    return (porcentaje / 100) * monto;
+}
